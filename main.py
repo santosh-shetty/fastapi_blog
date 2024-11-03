e430aaea0dbb410a81c2b3a9cc6a17ec411be5e3
santosh-shetty <santosh@hexagn.in>
2024-11-03 10:35:57 +0530

    cors fixed

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ import schemas
 import os
 import uuid
 from sqlalchemy.exc import IntegrityError
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Initialize the application
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create the database tables
 Base.metadata.create_all(engine)
